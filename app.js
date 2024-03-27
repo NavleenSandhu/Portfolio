67120d3e602b262631f988c5a7055f9f22b687a4
NavleenSandhu <sandnavl@shernet.sheridancollege.ca>
2024-03-27 10:51:23 -0400

    first commit

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 (function () {
     [...document.querySelectorAll(".control")].forEach(button => {
-        button.addEventListener("click", function() {
-            document.querySelector(".active-btn").classList.remove("active-btn");
+        button.addEventListener("click", function () {
+            document.querySelectorAll(".active-btn").forEach((btn) => {
+                btn.classList.remove("active-btn");
+            })
             this.classList.add("active-btn");
             document.querySelector(".active").classList.remove("active");
             document.getElementById(button.dataset.id).classList.add("active");
@@ -9,5 +11,37 @@
     });
     document.querySelector(".theme-btn").addEventListener("click", () => {
         document.body.classList.toggle("light-mode");
+    })
+    const readMoreBtns = document.querySelectorAll(".read-more-btn")
+
+    for (let i = 0; i < readMoreBtns.length; i++) {
+        readMoreBtns[i].addEventListener('click', () => {
+            let readMoreSection = document.querySelector(`#read-more-${i}`)
+            readMoreSection.classList.toggle('read-more-active')
+            readMoreSection.classList.toggle('read-more')
+        })
+    }
+    const collapseBtns = document.querySelectorAll(".read-more div a>button")
+    for (let i = 0; i < collapseBtns.length; i++) {
+        collapseBtns[i].addEventListener('click', () => {
+            let readMoreSection = document.querySelector(`#read-more-${i}`)
+            readMoreSection.classList.toggle('read-more-active')
+            readMoreSection.classList.toggle('read-more')
+        })
+    }
+    const sendMailBtn = document.querySelector('#sendMail')
+    sendMailBtn.addEventListener('click', async () => {
+        console.log(document.querySelector('.i-c-2 input[type="email"]').value)
+        await fetch('http://srv485884.hstgr.cloud:5200', {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                "from": document.querySelector('.i-c-2 input[type="email"]').value,
+                "subject": document.querySelector("#subject").value,
+                "text": document.querySelector('.input-control textarea').value
+            })
+        })
     })
 })();
